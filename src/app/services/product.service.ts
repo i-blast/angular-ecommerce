a136306bf03ad8272cfcd1f4b5ce624967702837
ilYa <i.blast59@gmail.com>
2022-03-11 00:54:57 +0300

    feat(product-list): Add pagination for products search component

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -50,6 +50,18 @@ export class ProductService {
     return this.getProducts(searchUrl);
   }
 
+  searchProductsPaginate(
+    thePage: number,
+    thePageSize: number,
+    theKeyword: string
+  ): Observable<GetResponseProducts> {
+    const searchUrl =
+      `${this.baseUrl}/search/findByNameContaining?name=${theKeyword}` +
+      `&page=${thePage}&size=${thePageSize}`;
+
+    return this.httpClient.get<GetResponseProducts>(searchUrl);
+  }
+
   getProduct(productId: number): Observable<Product> {
     const productUrl = `${this.baseUrl}/${productId}`;
 
