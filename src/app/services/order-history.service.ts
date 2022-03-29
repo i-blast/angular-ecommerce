7336efc9448b5866f45a6a8dd7d77f154dfa0e5c
ilYa <i.blast59@gmail.com>
2022-03-30 00:15:02 +0300

    feat(order-history): Sort orders by date created

--- src/app/services/order-history.service.ts
+++ src/app/services/order-history.service.ts
@@ -7,13 +7,12 @@ import { OrderHistory } from '../common/order-history';
   providedIn: 'root'
 })
 export class OrderHistoryService {
-
   private orderUrl = 'http://localhost:8080/api/orders';
 
   constructor(private httpClient: HttpClient) { }
 
   getOrderHistory(email: string): Observable<GetResponseOrderHistory> {
-    const orderHistoryUrl = `${this.orderUrl}/search/findByCustomerEmail?email=${email}`;
+    const orderHistoryUrl = `${this.orderUrl}/search/findByCustomerEmailOrderByDateCreatedDesc?email=${email}`;
 
     return this.httpClient.get<GetResponseOrderHistory>(orderHistoryUrl);
   }
