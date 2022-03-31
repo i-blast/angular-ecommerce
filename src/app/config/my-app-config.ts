f0883ab72962cfa875aa7b5b6efc8dfba99174ef
ilYa <i.blast59@gmail.com>
2022-03-31 23:43:14 +0300

    feat(security): Move frontend to http secure protocol

--- src/app/config/my-app-config.ts
+++ src/app/config/my-app-config.ts
@@ -2,7 +2,7 @@ export default {
   oidc: {
     clientId: '0oa4djqf7sPKbr0qQ5d7',
     issuer: 'https://dev-27985776.okta.com/oauth2/default',
-    redirectUri: 'http://localhost:4200/login/callback',
+    redirectUri: 'https://localhost:4200/login/callback',
     scopes: ['openid', 'profile', 'email'],
   },
 };
